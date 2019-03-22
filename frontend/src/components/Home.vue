227407809fdfb8919ac2f58783663c9f3cd813e3
Martijn Koch <martijn_waterreus@hotmail.com>
2019-03-22 12:36:51 +0100

    Sidebar icons added and sidebar component created

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,42 +1,26 @@
 <template>
   <div id="app">
     <div class="wrapper">
-        <!-- Sidebar -->
-        <nav id="sidebar">
-             <div class="sidebar-header">
-                <img src="@/assets/images/Maerschalk_logo.svg"/>
-            </div>
-            <ul class="list-unstyled components">
-            <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Dashboard</a>
-            </li>
-            <li>
-                <a href="#">Email</a>
-            </li>
-            <li>
-                <a href="#">Meldingen</a>
-            </li>
-            <li>
-                <a href="#">Team</a>
-            </li>
-        </ul>
-        </nav>
+        <Sidebarcomponent></Sidebarcomponent>
 
         <!-- Page Content -->
         <div id="content">
-            <!-- We'll fill this with dummy content -->
+            Piemol<br>
+            Emo<br>
+            Rukker<br>
+            Urk<br>
         </div>
     </div>       
 </div>
 </template>
 
 <script>
-
+import Sidebarcomponent from './Sidebarcomponent.vue'
 
 export default {
   name: 'app',
   components: {
-    
+    'Sidebarcomponent': Sidebarcomponent,
   }
 }
 </script>
---
+++ frontend/src/components/Sidebarcomponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Sidebar -->
+    <nav id="sidebar">
+            <div class="sidebar-header">
+            <img src="@/assets/images/Maerschalk_logo.svg"/>
+        </div>
+        <ul class="list-unstyled components">
+            <div class="boder">
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Dashboard</a>
+                </li>
+            </div>
+            <li>
+                <a href="#">Email</a>
+            </li>
+            <li>
+                <a href="#">Meldingen</a>
+            </li>
+            <li>
+                <a href="#">Team</a>
+            </li>
+        </ul>
+    </nav>
+</template>
