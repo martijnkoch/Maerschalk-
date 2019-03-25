c6eda546a265b7953d8537221b1a11b8fe8ce4c1
Martijn Koch <martijn_waterreus@hotmail.com>
2019-03-25 16:13:17 +0100

    New MYSQL table for emergency calls

---
+++ backend/_sql/emergency_call.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2016 at 04:32 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `slimapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergency_calls`
+--
+
+CREATE TABLE `emergency_calls` (
+  `id` int(11) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `emergency_calls`
+--
+
+INSERT INTO `emergency_calls` (`id`, `company_name`, `phone`) VALUES
+(1, 'Royal Canin', '333-333-3333'),
+(2, 'Simac', '333-333-3333');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emergency_calls`
+--
+ALTER TABLE `emergency_calls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emergency_calls`
+--
+ALTER TABLE `emergency_calls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
