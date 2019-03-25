10def0c173ce92fe2361f1d245fae63d5563de3f
Yinghing <yinghing.lam@hotmail.com>
2019-03-25 14:33:53 +0100

    Update card dashboard

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,21 +2,56 @@
 <div class="container-fluid header-container">
     <Headercomponent></Headercomponent>
 
-    <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
+<b-container class="bv-example-row mt-7">
+   
+    <h1>Noodoproepen</h1>
+   
+    <b-row>
+        <b-col>
+            <b-card
+            title="Royal Canin"
+            tag="article"
+            style="max-width: 25rem;"
+            >
 
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
+            <b-card-text>Team development</b-card-text>
+            <b-card-text>06 12 34 56 78</b-card-text>
+
+            <b-button href="#" class="float-right" variant="primary">New</b-button>
+            </b-card>
+        </b-col>
+        
+        <b-col>
+            <b-card
+            title="Makita"
+            tag="article"
+            style="max-width: 25rem;"
+            >
+
+            <b-card-text>Team development</b-card-text>
+            <b-card-text>06 12 34 56 78</b-card-text>
+
+            <b-button href="#" class="float-right" variant="primary">New</b-button>
+            </b-card>
+        </b-col>
+        
+        <b-col>
+            <b-card
+            title="Card Title"
+            tag="article"
+            style="max-width: 25rem;"
+            >
+
+            <b-card-text>Team development</b-card-text>
+            <b-card-text>06 12 34 56 78</b-card-text>
+
+            <b-button href="#" class="float-right" variant="primary">New</b-button>
+            </b-card>
+        </b-col>
+    </b-row>
+</b-container>
+   
+    
 </div>
 </template>
 
