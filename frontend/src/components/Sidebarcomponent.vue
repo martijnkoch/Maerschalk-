4cedd2bc8499ee23ce6ea3fe553d7d342f419633
Yinghing <yinghing.lam@hotmail.com>
2019-03-27 13:27:50 +0100

    Back to dashboard

--- frontend/src/components/Sidebarcomponent.vue
+++ frontend/src/components/Sidebarcomponent.vue
@@ -2,12 +2,14 @@
     <!-- Sidebar -->
     <nav id="sidebar">
             <div class="sidebar-header">
-            <img src="@/assets/images/Maerschalk_logo.svg"/>
+            <router-link to="/">
+                <img src="@/assets/images/Maerschalk_logo.svg"/>
+            </router-link>
         </div>
         <ul class="list-unstyled components">
             <div class="boder">
                 <li class="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Dashboard</a>
+                    <a href="/" data-toggle="collapse" aria-expanded="false">Dashboard</a>
                 </li>
             </div>
             <li>
