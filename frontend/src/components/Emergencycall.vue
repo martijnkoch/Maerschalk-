b83496c9d3c7f20ac7bed7bdd0f2d3484c146c2d
Yinghing <yinghing.lam@hotmail.com>
2019-04-01 12:24:27 +0200

    CSS emergency call & popup

--- frontend/src/components/Emergencycall.vue
+++ frontend/src/components/Emergencycall.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="container-fluid header-container">
       <Headercomponent></Headercomponent>
-      <b-container class="bv-example-row mt-7">
-        <router-link to="/"><b-button variant="link">Go Back</b-button></router-link>
-        <h2>{{call.company_name}}</h2>
-        <h2>{{call.phone}}</h2>
-        <div class="form-group shadow-textarea">
-          <textarea class="form-control call-textarea" rows="5" v-model="call.body"></textarea>
+      <b-container class="bv-example-row mt-5">
+       <div class="d-flex noodoproep-title">
+        <div class="py-2 pr-2"><h2>{{call.company_name}}</h2></div>
+        <div class="mr-auto p-2 mt-1"><h4>{{call.phone }}</h4></div>
+        <div class="p-2 mt-1"><h4>{{call.phone}}</h4></div>
+        <div class="py-2 pl-2"><b-button class="homepage-cardbutton mt-1" variant="warning" :class="[call.status.toLowerCase()]">{{call.status}}</b-button></div>
         </div>
-        <b-button class="mr-3" variant="primary" v-on:click="updateCall(call.id)">Update</b-button>
-        <b-button v-b-modal.modal-center variant="danger" v-b-modal.modal-1>Verwijder</b-button>
+        <div class="form-group shadow-textarea">
+          <textarea class="form-control call-textarea mt-3 mw-100" rows="15" v-model="call.body"></textarea>
+        </div>
+        <div class="d-flex justify-content-end">
+        <router-link to="/"><b-button variant="link" class="text-secondary">Ga terug</b-button></router-link>
+        <b-button class="mr-3 text-white" variant="warning" v-on:click="updateCall(call.id)">Update</b-button>
+        <b-button v-b-modal.modal-center variant="success" v-b-modal.modal-1>Klaar</b-button>
+        </div>
         <!-- Modal Component -->
-        <b-modal hide-footer id="modal-center" size="lg" centered title="Wijziging" ref="emergency-modal">
-          <p class="my-4">Weet je zeker dat je de call wil deleten?</p>
+        <b-modal hide-footer id="modal-center" centered title="Klaar" ref="emergency-modal" data-backdrop="static" data-keyboard="false" class="no-border">
+          <p class="my-4">Weet je zeker dat je de noodoproep wil verwijderen?</p>
+          <div class="d-flex justify-content-end">
           <b-button class="mr-3" v-on:click="hideModal">Annuleren</b-button>
-          <b-button variant="danger" v-on:click="deleteCall(call.id)">Delete</b-button>
+          <b-button variant="success" v-on:click="deleteCall(call.id)">Klaar</b-button>
+            </div>
         </b-modal>
       </b-container>
   </div>
@@ -64,8 +72,12 @@ export default {
                 this.$router.push({path: '/'});
             });
       e.preventDefault();
-      }
+      },
+            hideModal() {
+        this.$refs['emergency-modal'].hide()
+      },
     },
+    
   created: function(){
     this.fetchCall(this.$route.params.id);
   }
