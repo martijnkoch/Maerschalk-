fab0ac46e692c71a08721bfc5cd8f869130b15ef
Yinghing <yinghing.lam@hotmail.com>
2019-04-01 12:53:43 +0200

    CSS emergency call

--- frontend/src/components/Emergencycall.vue
+++ frontend/src/components/Emergencycall.vue
@@ -2,18 +2,18 @@
   <div class="container-fluid header-container">
       <Headercomponent></Headercomponent>
       <b-container class="bv-example-row mt-5">
-       <div class="d-flex noodoproep-title">
+       <div class="d-flex noodoproep-title align-bottom">
         <div class="py-2 pr-2"><h2>{{call.company_name}}</h2></div>
-        <div class="mr-auto p-2 mt-1"><h4>{{call.phone }}</h4></div>
-        <div class="p-2 mt-1"><h4>{{call.phone}}</h4></div>
-        <div class="py-2 pl-2"><b-button class="homepage-cardbutton mt-1" variant="warning" :class="[call.status.toLowerCase()]">{{call.status}}</b-button></div>
+        <div class="mr-auto p-2 mt-9"><h5>{{call.phone }}</h5></div>
+        <div class="p-2 mt-9"><h5>{{call.date}}</h5></div>
+        <div class="py-2 pl-2 mt-1"><b-button class="homepage-cardbutton" variant="warning" :class="[call.status.toLowerCase()]">{{call.status}}</b-button></div>
         </div>
         <div class="form-group shadow-textarea">
           <textarea class="form-control call-textarea mt-3 mw-100" rows="15" v-model="call.body"></textarea>
         </div>
         <div class="d-flex justify-content-end">
         <router-link to="/"><b-button variant="link" class="text-secondary">Ga terug</b-button></router-link>
-        <b-button class="mr-3 text-white" variant="warning" v-on:click="updateCall(call.id)">Update</b-button>
+        <b-button class="mr-3 text-white" variant="primary" v-on:click="updateCall(call.id)">Update</b-button>
         <b-button v-b-modal.modal-center variant="success" v-b-modal.modal-1>Klaar</b-button>
         </div>
         <!-- Modal Component -->
